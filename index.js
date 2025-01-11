e59e58d4f338bb84cacb374f1b5a91e2a49ba1bd
たいち ひ <taichi221228@icloud.com>
2025-01-11 15:53:07 +0900

    feat: Add Message typedef and apply type annotations
    Introduced a `Message` typedef in a new file to define valid values as "Hello" or "你好".

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello via Bun!");
+
+/** @import { Message } from "./typedef" */
+
+/**
+ * @satisfies {Message}
+ */
+const _message = "Hello";
+
+/**
+ * @type {Message}
+ */
+const _messageUsingType = "Hello";
---
+++ typedef.js
@@ -0,0 +1,1 @@
+/** @typedef {"Hello" | "你好"} Message */
